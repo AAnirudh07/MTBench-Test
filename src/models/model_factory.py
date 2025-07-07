fb952197ce1a185e8a62874ec92a3dccfa81efe7
AAnirudh07 <anirudh19015@cse.ssn.edu.in>
2025-07-07 10:01:55 +0530

    Finish model factory

--- src/models/model_factory.py
+++ src/models/model_factory.py
@@ -1,5 +1,6 @@
 from base_model import BaseModel
 from llama_model import LLaMAModel
+from deepseek_model import DeepSeekModel
 
 class ModelFactory:
     def __init__(self, config: dict):
@@ -7,7 +8,7 @@ class ModelFactory:
 
     def get_model(self, model_type: str, model_name: str, **kwargs) -> BaseModel:
         if model_type == "deepseek":
-            pass
+            return DeepSeekModel(model_name=model_name, **kwargs)
         elif model_type == "llama":
             return LLaMAModel(model_name=model_name, **kwargs)
         else:
