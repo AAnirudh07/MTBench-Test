150f1b120b8cfd179f9054421ae487df8ff34706
AAnirudh07 <anirudh19015@cse.ssn.edu.in>
2025-07-07 10:01:49 +0530

    Add deepseek model code

---
+++ src/models/deepseek_model.py
@@ -0,0 +1,37 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from base_model import BaseModel
+
+class DeepSeekModel(BaseModel):
+    def __init__(self, model_name: str = "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B", **kwargs):
+        
+        # The model is set in eval mode by default by using eval()
+        # See: https://huggingface.co/docs/transformers/en/main_classes/model#transformers.PreTrainedModel
+        self.model = AutoModelForCausalLM.from_pretrained(
+            model=model_name,
+            torch_dtype="auto", 
+            device_map="auto",
+            **kwargs
+        )
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    def inference(self, content: str) -> str:
+        messages = [{"role": "user", "content": content}]
+
+        chat_prompt = self.tokenizer.apply_chat_template(
+            messages,
+            tokenize=False,
+            add_generation_prompt=True
+        )
+        tokenized_input = self.tokenizer([chat_prompt], return_tensors="pt").to(self.model.device)
+        generated_output = self.model.generate(
+            **tokenized_input,
+            max_new_tokens=4096,
+            pad_token_id=self.tokenizer.eos_token_id
+        )
+        new_tokens = [
+            output[len(input_ids):]
+            for input_ids, output in zip(tokenized_input.input_ids, generated_output)
+        ]
+        outputs = self.tokenizer.batch_decode(new_tokens, skip_special_tokens=True)[0]
+
+        return outputs
